d619eec0a48004ddf4bc931fc5a0a1c56a536987
iii <miselrkba@gmail.com>
2020-10-15 19:00:50 +0200

    added contacts to show on state change

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Constants from 'expo-constants'
+import React from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
 
-import contacts from './contacts'
+import contacts from "./contacts";
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
-  }
+  };
 
   toggleContacts = () => {
-    this.setState(prevState => ({showContacts: !prevState.showContacts}))
-  }
+    this.setState((prevState) => ({ showContacts: !prevState.showContacts }));
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        {this.state.showContacts ? (
+          <ScrollView>
+            {contacts.map((contact) => (
+              <View style={styles.row} key={contact.key}>
+                <Text>{contact.name}</Text>
+                <Text>{contact.phone}</Text>
+              </View>
+            ))}
+          </ScrollView>
+        ) : null}
       </View>
     );
   }
@@ -25,7 +35,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: Constants.statusBarHeight,
+  },
+  row: {
+    padding: 20,
   },
 });
